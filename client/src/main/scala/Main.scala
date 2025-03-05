6b7a9b42d8dc7ed5c4dadd1d033541a72de3c931
Bill Frasure <bill.frasure@gmail.com>
2025-03-04 22:43:18 -0700

    Headless UI dev

--- client/src/main/scala/Main.scala
+++ client/src/main/scala/Main.scala
@@ -1,33 +1,54 @@
 import com.raquo.laminar.api.L.{*, given}
 import org.scalajs.dom
 
+case class Discussion(topic: String)
+
+def TopicSubmission(submitEffect: Observer[Discussion]) =
+  val intBus = new EventBus[Int]
+  val textVar = Var("")
+  div(
+    input(
+      onClick.mapTo(1) --> intBus,
+      value <-- textVar,
+      onInput.mapToValue --> textVar,
+      onMouseOver --> { ev => println(ev) },
+      onChange --> { _ => println("committed") },
+      onBlur.mapTo("blur") --> Observer {blurValue => println(blurValue)}
+    ),
+    button(
+      onClick.mapTo(textVar.now()).map(Discussion.apply) --> submitEffect,
+      "Submit"
+    )
+  )
+
 object Main extends App:
     lazy val container = dom.document.getElementById("app")
     import io.laminext.websocket._
 
+
     val ws = WebSocket.url("wss://echo.websocket.org").string.build()
+
+    val topicsToReview: Var[List[Discussion]] =
+      Var(List(Discussion("Scala 3")))
+
+    val submitNewTopic: Observer[Discussion] = Observer {
+      discussion =>
+        ws.sendOne(discussion.toString) // TODO Json
+    }
+
     val app = {
       println("should have a ws?")
       div(
-
         ws.connect,
         ws.received --> Observer {
           event =>
           println("From WS: " + event)
         } ,
+        TopicSubmission(submitNewTopic),
         button(
-          onClick --> Observer {
-            clickyEvent =>
-              println("Should be sending a message")
-              ws.sendOne("a message")
-          },
-          "Click me"
-        ),
-        button(
-          onClick.mapTo("boop") --> ws.send,
+          onClick.mapTo("boop_" + scala.util.Random.nextInt()) --> ws.send,
           "Click me better"
         ),
-        "Replaced"
       )
     }
     println("Doing app now???")
