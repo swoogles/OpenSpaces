a2129950eb2440233a43a1cea42c7687ed79ec1a
Bill Frasure <bill.frasure@gmail.com>
2025-03-05 08:58:11 -0700

    All clients getting the same updates :D :D :D

--- server/src/main/scala/co/wtf/openspaces/Backend.scala
+++ server/src/main/scala/co/wtf/openspaces/Backend.scala
@@ -6,7 +6,7 @@ import zio.http.*
 import zio.json.*
 
 val randomDiscussion =
-  for 
+  for
     id <- Random.nextIntBounded(10)
   yield Discussion("Blah_" + id)
 
@@ -19,62 +19,86 @@ object Backend extends ZIOAppDefault {
   import zio.http._
   import zio.http.codec.PathCodec.string
 
-  val socketApp: WebSocketApp[Any] =
-    Handler.webSocket { channel =>
-      channel.receiveAll {
-        case Read(WebSocketFrame.Text("end")) =>
-          channel.shutdown
+  case class ApplicationState(channels: Ref[List[WebSocketChannel]]):
 
-        // Send a "bar" if the client sends a "foo"
-        case Read(WebSocketFrame.Text("foo")) =>
-          channel.send(Read(WebSocketFrame.text("bar")))
+    val socketApp: WebSocketApp[Any] =
+      Handler.webSocket { channel =>
+        channel.receiveAll {
+          case Read(WebSocketFrame.Text("end")) =>
+            channel.shutdown
 
-        // Send a "foo" if the client sends a "bar"
-        case Read(WebSocketFrame.Text("bar")) =>
-          channel.send(Read(WebSocketFrame.text("foo")))
+          // Send a "bar" if the client sends a "foo"
+          case Read(WebSocketFrame.Text("foo")) =>
+            channel.send(Read(WebSocketFrame.text("bar")))
 
-        // Echo the same message 10 times if it's not "foo" or "bar"
-        case Read(WebSocketFrame.Text(text)) =>
-          channel
-            .send(Read(WebSocketFrame.text(s"echo $text")))
-            .repeatN(10)
-            .catchSomeCause { case cause =>
-              ZIO.logErrorCause(s"failed sending", cause)
-            }
+          // Send a "foo" if the client sends a "bar"
+          case Read(WebSocketFrame.Text("bar")) =>
+            channel.send(Read(WebSocketFrame.text("foo")))
 
-        // Send a "greeting" message to the client once the connection is established
-        case UserEventTriggered(UserEvent.HandshakeComplete) =>
-          println("Should send greetings")
-          (for {
-            discussion <- randomDiscussion
-            _ <- channel.send(Read(WebSocketFrame.text(discussion.toJson)))
-          } yield ()).repeat(Schedule.spaced(5.seconds) && Schedule.forever)
-          
+          // Echo the same message 10 times if it's not "foo" or "bar"
+          case Read(WebSocketFrame.Text(text)) =>
+            channel
+              .send(Read(WebSocketFrame.text(s"echo $text")))
+              .repeatN(10)
+              .catchSomeCause { case cause =>
+                ZIO.logErrorCause(s"failed sending", cause)
+              }
 
-        // Log when the channel is getting closed
-        case Read(WebSocketFrame.Close(status, reason)) =>
-          Console.printLine("Closing channel with status: " + status + " and reason: " + reason)
+          // Send a "greeting" message to the client once the connection is established
+          case UserEventTriggered(UserEvent.HandshakeComplete) =>
+            channels.update(_ :+ channel) *>
+            Console.printLine("Should send greetings")
 
-        // Print the exception if it's not a normal close
-        case ExceptionCaught(cause) =>
-          Console.printLine(s"Channel error!: ${cause.getMessage}")
 
-        case _ =>
-          ZIO.unit
+          // Log when the channel is getting closed
+          case Read(WebSocketFrame.Close(status, reason)) =>
+            Console.printLine("Closing channel with status: " + status + " and reason: " + reason)
+
+          // Print the exception if it's not a normal close
+          case ExceptionCaught(cause) =>
+            Console.printLine(s"Channel error!: ${cause.getMessage}")
+
+          case _ =>
+            ZIO.unit
+        }
       }
-    }
 
-  val socketRoutes =
-    Routes(
-      Method.GET / "discussions"          -> handler(socketApp.toResponse)
-    )
+    val socketRoutes =
+      Routes(
+        Method.GET / "discussions"          -> handler(socketApp.toResponse)
+      )
+
+  object ApplicationState:
+    val layer =
+      ZLayer.fromZIO:
+        for {
+          state <- Ref.make(List.empty[WebSocketChannel])
+          _ <-
+            (for {
+              discussion <- randomDiscussion
+              channels <- state.get
+              _ <-
+                ZIO.foreachDiscard(channels)( channel =>
+                  channel.send(Read(WebSocketFrame.text(discussion.toJson)))
+                )
+            } yield ()).repeat(Schedule.spaced(5.seconds) && Schedule.forever).forkDaemon
+        } yield ApplicationState(state)
+
 
   /**
    * Creates an HTTP app that only serves static files from resources via
    * "/static". For paths other than the resources directory, see
    * [[zio.http.Middleware.serveDirectory]].
    */
-  val routes = socketRoutes @@ Middleware.serveResources(Path.empty)
+//  val routes = socketRoutes
 
-  override def run = Server.serve(routes).provide(Server.default)
+  override def run =
+    (for {
+      statefulRoutes <- ZIO.serviceWith[ApplicationState](_.socketRoutes)
+      _ <- Server.serve(statefulRoutes @@ Middleware.serveResources(Path.empty))
+    } yield ())
+    .provide(
+      Server.default,
+      ApplicationState.layer
+    )
 }
