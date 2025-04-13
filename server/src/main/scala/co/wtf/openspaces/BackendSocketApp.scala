e7f5fad2665da121b6c8b749edae338d6d080e36
Bill Frasure <bill.frasure@gmail.com>
2025-04-12 23:23:24 -0600

    Faster random

--- server/src/main/scala/co/wtf/openspaces/BackendSocketApp.scala
+++ server/src/main/scala/co/wtf/openspaces/BackendSocketApp.scala
@@ -68,7 +68,8 @@ case class BackendSocketApp(
                             .run
                         .repeat(
                           Schedule.spaced(
-                            1.seconds,
+                            500.millis,
+                            // 1.seconds,
                           ) && Schedule.forever,
                         )
                       .forkDaemon
