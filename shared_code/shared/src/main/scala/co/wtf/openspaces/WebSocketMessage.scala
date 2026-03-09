32b8a01745f6d9b29b53fa63218fec0bc005f371
Bill Frasure <bill.frasure@gmail.com>
2026-03-08 22:10:48 -0600

    Delete a ton of redundant JsonCodecs

--- shared_code/shared/src/main/scala/co/wtf/openspaces/WebSocketMessage.scala
+++ shared_code/shared/src/main/scala/co/wtf/openspaces/WebSocketMessage.scala
@@ -42,42 +42,34 @@ case class Ticket(
 case class DiscussionActionMessage(
   action: DiscussionAction)
     extends WebSocketMessageFromClient
-    derives JsonCodec
 
 case class DiscussionActionConfirmedMessage(
   event: DiscussionActionConfirmed)
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 case class LightningTalkActionMessage(
   action: LightningTalkAction)
     extends WebSocketMessageFromClient
-    derives JsonCodec
 
 case class LightningTalkActionConfirmedMessage(
   event: LightningTalkActionConfirmed)
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 case class HackathonProjectActionMessage(
   action: HackathonProjectAction)
     extends WebSocketMessageFromClient
-    derives JsonCodec
 
 case class HackathonProjectActionConfirmedMessage(
   event: HackathonProjectActionConfirmed)
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 case class ActivityActionMessage(
   action: ActivityAction)
     extends WebSocketMessageFromClient
-    derives JsonCodec
 
 case class ActivityActionConfirmedMessage(
   event: ActivityActionConfirmed)
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 // Authorization messages
 
@@ -85,19 +77,16 @@ case class ActivityActionConfirmedMessage(
 case class AuthorizationStatusMessage(
   status: AuthorizationStatus)
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 /** Sent from client (admin) to server to manage user authorization */
 case class AuthorizationActionMessage(
   action: AuthorizationAction)
     extends WebSocketMessageFromClient
-    derives JsonCodec
 
 /** Broadcast from server when authorization changes */
 case class AuthorizationActionConfirmedMessage(
   event: AuthorizationActionConfirmed)
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 // Authorization domain types
 
@@ -141,26 +130,22 @@ enum AuthorizationActionConfirmed derives JsonCodec:
 case class LocationActionMessage(
   action: LocationAction)
     extends WebSocketMessageFromClient
-    derives JsonCodec
 
 /** Broadcast from server when location state changes */
 case class LocationActionConfirmedMessage(
   event: LocationActionConfirmed)
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 // Slack reply counts
 
 /** Lightweight heartbeat frame to keep websocket connections from going idle */
 case object KeepAliveMessage
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 /** Broadcast from server with updated Slack thread reply counts */
 case class SlackReplyCountsMessage(
   counts: SlackReplyCounts)
     extends WebSocketMessageFromServer
-    derives JsonCodec
 
 /** Reply counts for all entity types, keyed by entity ID (as String for JSON compatibility) */
 case class SlackReplyCounts(
